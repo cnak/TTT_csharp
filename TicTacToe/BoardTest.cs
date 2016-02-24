753657ce6c19725626e1eba2fe9aa8c0ea254973
Cedric Kisema <cedric.kisema@gmail.com>
2016-02-24 13:33:48 +0000

    informs Game Is Not Over With MovesLeft test

--- TicTacToe/BoardTest.cs
+++ TicTacToe/BoardTest.cs
@@ -1,37 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using NUnit.Framework;
 
-namespace TicTacToe {
-
+namespace TicTacToe
+{
     [TestFixture]
     class BoardTest
     {
         [Test]
-        public void ReturnsFalseForGameOverOnANewGame() { 
+        public void informsTheGameIsNotOver()
+        {
             Board board = new Board();
+
             Assert.IsFalse(board.IsGameOver());
         }
 
         [Test]
-        public void ReturnsTrueWhenAllMovesPlayed()
+        public void informsTheGameIsOverWithNoMoreMoves()
         {
-            var boardPositions = new Dictionary<string, string>()
-            {
-                {"topLeft", "X"},
-                {"topCenter", "O"},
-                {"topRight", "X"},
-                {"middle-left", "O"},
-                {"middle-center ", "O"},
-                {"middle-right ", "X"},
-                {"bottom-left ", "O"},
-                {"bottom-center ", "X"},
-                {"bottom-right ", "O"}
-            };
-            
-            Board board = new Board(boardPositions);
+            Board board = new Board("XOXOXOXOX");
+
             Assert.IsTrue(board.IsGameOver());
         }
-    }
+
+        [Test]
+        public void informsGameIsNotOverWithMovesLeft()
+        {
+            Board board = new Board("XOXO");
+
+            Assert.IsFalse(board.IsGameOver());
+        }
+   }
 }
