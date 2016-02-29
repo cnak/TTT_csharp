1cb307e357a254711faa0e013bd1d2cfb8e6f0fb
Cedric <kisema@gmail.com>
2016-02-29 21:48:08 +0000

    toggle current player mark on move is played

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -9,19 +9,47 @@ namespace TicTacToe
     public class Game
     {
         private IGameConsole console;
+        private Board board;
+        private char currentPlayerMark = 'X';
 
         public Game(IGameConsole console)
         {
             this.console = console;
+            this.board = new Board();
         }
 
-        public void Start() {
+        public void Start()
+        {
             console.DisplayBoard();
         }
 
         public void AskForInputPosition()
         {
-            console.Write("Please Enter Input");
+            console.Write("Please play a move");
+        }
+
+        public void PlayMove(int position)
+        {
+            this.board.MakeMove(position, currentPlayerMark);
+            ToggleCurrentPlayerMark();
+        }
+
+        private void ToggleCurrentPlayerMark()
+        {
+            if (currentPlayerMark == 'X')
+            {
+                currentPlayerMark = 'O';
+            }
+            else
+            {
+                currentPlayerMark = 'X';
+            }
+        }
+
+
+        public object PositionAt(int position)
+        {
+            return board.PositionAt(position);
         }
     }
 }
