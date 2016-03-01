33b7f25a58291fb1f2cd9731473d86cfbff05d97
Cedric <kisema@gmail.com>
2016-03-01 02:04:41 +0000

    console displays empty board

---
+++ TicTacToe/GameConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameConsole
+    {
+        private readonly TextWriter writer;
+
+        private string board = "-------\n|1|2|3|\n-------\n|4|5|6|\n-------\n|7|8|9|\n-------\n";
+
+        public GameConsole(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void DisplayBoard()
+        {
+            writer.Write(board);
+            writer.Flush();
+        }
+    }
+}
---
+++ TicTacToe/GameConsoleTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TicTacToe
+{
+    [TestFixture]
+    public class GameConsoleTest
+    {
+        private GameConsole gameConsole;
+
+        private readonly string emptyBoard = "-------\n|1|2|3|\n-------\n|4|5|6|\n-------\n|7|8|9|\n-------\n";
+
+        [Test]
+        public void DisplaysEmptyTheBoard()
+        {
+            MemoryStream stream = new MemoryStream();
+            gameConsole = new GameConsole(new StreamWriter(stream));
+
+            gameConsole.DisplayBoard();
+
+            StreamReader sr = new StreamReader(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            Assert.AreEqual(emptyBoard, sr.ReadToEnd());
+        }
+    }
+}
