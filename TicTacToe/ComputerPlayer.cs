ef534c0f01f3c3af582b2b6a911d6651ae993ef7
Cedric <kisema@gmail.com>
2016-03-08 14:33:03 +0000

    computer makes a move on an empty space

--- TicTacToe/ComputerPlayer.cs
+++ TicTacToe/ComputerPlayer.cs
@@ -2,5 +2,24 @@
 {
     public class ComputerPlayer : IPlayer
     {
+        public int GetMove(Board board)
+        {
+            var bestMove = -1;
+            var isFoundMove = false;
+
+            for (var index = 0; index < 9 && !isFoundMove; index++)
+            {
+                bestMove = FindMoveOnEmptyPosition(board, index, bestMove, ref isFoundMove);
+            }
+            return bestMove;
+        }
+
+        private int FindMoveOnEmptyPosition(Board board, int index, int bestMove, ref bool isFoundMove)
+        {
+            if (!board.IsEmptyPosition(index)) return bestMove;
+            bestMove = index;
+            isFoundMove = true;
+            return bestMove;
+        }
     }
 }
