b8417876412f46e5f9a98ae8d43d8601b9bb0918
Cedric <kisema@gmail.com>
2016-03-17 14:15:33 +0000

    score method complete

--- TicTacToe/GameLogic/Minimax.cs
+++ TicTacToe/GameLogic/Minimax.cs
@@ -1,10 +1,27 @@
-﻿namespace TicTacToe.xTests
+﻿using System.Runtime.InteropServices;
+
+namespace TicTacToe.xTests
 {
     public class Minimax
     {
-        public static double move(Board board)
+        public static double Move(Board board)
         {
             return 1;
+        }
+
+        public static int Score(Board board, int depth, string minimaxPlayer)
+        {
+            if (board.IsGameDrawn())
+                return 0;
+            if (AmITheWinner(board, minimaxPlayer))
+                return 10;
+
+            return -10;
+        }
+
+        private static bool AmITheWinner(Board board, string player)
+        {
+            return board.GetWinner() == player;
         }
     }
 }
--- TicTacToe/xTests/MinimaxTest.cs
+++ TicTacToe/xTests/MinimaxTest.cs
@@ -10,8 +10,46 @@ namespace TicTacToe.xTests
             var board = new Board("--X" +
                                   "XOX" +
                                   "-O-");
-            var best_move = Minimax.move(board);
+            var best_move = Minimax.Move(board);
             Assert.AreEqual(1, best_move);
+        }
+
+        [Test]
+        public void ReturnsLostScore()
+        {
+            var board = new Board("XOX" +
+                                  "OXO" +
+                                  "X--");
+            var minimaxPlayer = "O";
+
+            var score = Minimax.Score(board, 0, minimaxPlayer);
+            Assert.AreEqual(-10, score);
+        }
+
+        [Test]
+        public void ReturnsDrawnScore()
+        {
+            var board = new Board("XOX" +
+                                  "OXO" +
+                                  "OXO");
+
+            var minimaxPlayer = "O";
+
+            var score = Minimax.Score(board, 0, minimaxPlayer);
+            Assert.AreEqual(0, score);
+        }
+
+        [Test]
+        public void ReturnsWinningScore()
+        {
+            var board = new Board("-OX" +
+                                  "XOX" +
+                                  "-O-");
+
+            var minimaxPlayer = "O";
+
+            var score = Minimax.Score(board, 0, minimaxPlayer);
+            Assert.AreEqual(10, score);
         }
     }
 }
