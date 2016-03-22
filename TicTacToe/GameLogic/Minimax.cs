4b5ed5f1a3e8d3de139e502809f9df9a797b002b
Cedric Kisema <cedric.kisema@gmail.com>
2016-03-22 13:59:45 +0000

    minimax implementation

--- TicTacToe/GameLogic/Minimax.cs
+++ TicTacToe/GameLogic/Minimax.cs
@@ -1,15 +1,67 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
 
 namespace TicTacToe.xTests
 {
     public class Minimax
     {
-        public static double Move(Board board)
+        private string computerPlayer;
+        private int bestMoveChoice;
+
+        public int Move(Board board)
         {
-            return 1;
+            computerPlayer = board.CurrentPlayer();
+            CalcMinimax(board, 0, computerPlayer);
+
+            return bestMoveChoice;
         }
 
-        public static int Score(Board board, int depth, string minimaxPlayer)
+        private int CalcMinimax(Board board, int depth, string player)
+        {
+            if (board.IsGameOver())
+            {
+                return Score(board, depth, player);
+            }
+
+            var scores = new Dictionary<int, int>();
+
+            FetchPossibleScore(board, player, scores, depth++);
+
+            return MinMaxScore(player, scores);
+        }
+
+        private void FetchPossibleScore(Board board, string player, IDictionary scores, int depth)
+        {
+            var available_spaces = board.GetRemainingMoveSpaces();
+            foreach (var space in available_spaces)
+            {
+                var possibleBoard = new Board(board);
+                var intMove = (int)space;
+                possibleBoard.MakeMove(intMove, player.ToCharArray()[0]);
+                var calculatedMiniMax = CalcMinimax(possibleBoard, depth, OpponentPlayer(board, player));
+                scores.Add(intMove, calculatedMiniMax);
+            }
+        }
+
+        private int MinMaxScore(string player, IDictionary<int, int> scores)
+        {
+            if (player == computerPlayer)
+                bestMoveChoice = scores.FirstOrDefault(x => x.Value == scores.Values.Min()).Key;
+                return scores.FirstOrDefault(x => x.Value == scores.Values.Max()).Key;
+
+                return scores.FirstOrDefault(x => x.Value == scores.Values.Max()).Key;
+        }
+
+        private string OpponentPlayer(Board board, string player)
+        {
+            return board.CurrentPlayer() == player ? board.OtherPlayer() : board.CurrentPlayer();
+//            return player == "X" ? "O" : "X";
+        }
+
+        public int Score(Board board, int depth, string minimaxPlayer)
         {
             if (board.IsGameDrawn())
                 return 0;
--- TicTacToe/xTests/MinimaxTest.cs
+++ TicTacToe/xTests/MinimaxTest.cs
@@ -5,12 +5,26 @@ namespace TicTacToe.xTests
     class MinimaxTest
     {
         [Test]
+        public void ShouldBlockTheOpponentByPlayingThree()
+        {
+            var board = new Board("XO-" +
+                                  "-XX" +
+                                  "OXO");
+//            var board = new Board("-X-" +
+//                                  "--X" +
+//                                  "OOX");
+//
+            Assert.AreEqual(3, (new Minimax().Move(board)));
+//            Assert.AreEqual(2, (new Minimax().Move(board)));
+        }
+
+        [Test]
         public void ReturnsOneAsTheWinningMove()
         {
             var board = new Board("--X" +
                                   "XOX" +
                                   "-O-");
-            var best_move = Minimax.Move(board);
+            var best_move = new Minimax().Move(board);
             Assert.AreEqual(1, best_move);
         }
 
@@ -22,7 +36,7 @@ namespace TicTacToe.xTests
                                   "X--");
             var minimaxPlayer = "O";
 
-            var score = Minimax.Score(board, 0, minimaxPlayer);
+            var score = new Minimax().Score(board, 0, minimaxPlayer);
             Assert.AreEqual(-10, score);
         }
 
@@ -35,7 +49,7 @@ namespace TicTacToe.xTests
 
             var minimaxPlayer = "O";
 
-            var score = Minimax.Score(board, 0, minimaxPlayer);
+            var score = new Minimax().Score(board, 0, minimaxPlayer);
             Assert.AreEqual(0, score);
         }
 
@@ -48,7 +62,7 @@ namespace TicTacToe.xTests
 
             var minimaxPlayer = "O";
 
-            var score = Minimax.Score(board, 0, minimaxPlayer);
+            var score = new Minimax().Score(board, 0, minimaxPlayer);
             Assert.AreEqual(10, score);
         }
     }
